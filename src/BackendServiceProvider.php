f6588a08ee675c64241a2fd024677d7ec10d7f60
Aung Phyo Pyae Kyaw <aungphyopyaekyaw@gmail.com>
2017-06-03 10:48:47 +0630

    second commit

--- src/BackendServiceProvider.php
+++ src/BackendServiceProvider.php
@@ -14,9 +14,12 @@ class BackendServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([ __DIR__.'/assets' => public_path('backend'), ], 'public');
-        $this->publishes([ __DIR__.'/views/backend' => base_path('resources/views/backend'), ]);
-        $this->publishes([ __DIR__.'/views/layouts' => base_path('resources/views/layouts'), ]);
-        $this->publishes([ __DIR__.'/views/auth' => base_path('resources/views/auth'), ]);
+        $this->publishes([
+          __DIR__.'/views/backend' => base_path('resources/views/backend'),
+          __DIR__.'/views/layouts' => base_path('resources/views/layouts'),
+          __DIR__.'/views/auth' => base_path('resources/views/auth'),
+
+        ]);
     }
 
     /**
