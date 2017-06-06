f9ebe6f253069aa5ba98abc1ca2cb5741e34aa9e
Aung Phyo Pyae Kyaw <aungphyopyaekyaw@gmail.com>
2017-06-06 10:21:45 +0630

    fixed url

--- src/BackendServiceProvider.php
+++ src/BackendServiceProvider.php
@@ -13,7 +13,7 @@ class BackendServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([ __DIR__.'/assets' => public_path('backend'), ], 'public');
+        $this->publishes([ __DIR__.'/assets' => public_path('asset'), ], 'public');
         $this->publishes([
           __DIR__.'/views/backend' => base_path('resources/views/backend'),
           __DIR__.'/views/layouts' => base_path('resources/views/layouts'),
